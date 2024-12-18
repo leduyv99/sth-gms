71428388cd93fe3e60c40a3b879f11bcfb1a2a3b
Đỗ Lê Duy <doleduy1999@gmail.com>
2024-12-18 16:48:56 +0700

    Init

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { Application } from "pixi.js";
+import './style.css'
+
+(async () => {
+  const app = new Application()
+
+  
+  await app.init({ resizeTo: window })
+  app.canvas.style.position = 'absolute'
+  
+  document.body.appendChild(app.canvas)
+})()
